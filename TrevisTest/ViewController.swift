0df557e7730eb541afcd6c4027700677f9458eae
Garnik Ghazaryan <garnik_ghazaryan@epam.com>
2017-12-05 18:29:25 +0400

    Initial Commit

---
+++ TrevisTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrevisTest
+//
+//  Created by Garnik Ghazaryan on 12/5/17.
+//  Copyright © 2017 Garnik Ghazaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
