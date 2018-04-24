f745031cbc37a1a4bfb5fb57434a28901529341f
Garnik Ghazaryan <garnik_ghazaryan@epam.com>
2018-04-24 21:18:24 +0400

    Tests added

--- TrevisTestTests/TrevisTestTests.swift
+++ TrevisTestTests/TrevisTestTests.swift
@@ -24,6 +24,7 @@ class TrevisTestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual("Text", "Text")
     }
     
     func testPerformanceExample() {
